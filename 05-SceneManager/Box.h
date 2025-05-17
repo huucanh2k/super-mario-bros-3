5d81013209d8c3c300a00e78d15242c1fe9dbffc
Vo Minh Vu <146900726+Vuvom1@users.noreply.github.com>
2025-05-17 14:07:13 +0700

    updaet box

--- 05-SceneManager/Box.h
+++ 05-SceneManager/Box.h
@@ -5,29 +5,33 @@
 // 
 // The most popular type of object in Mario! 
 // 
+
+#define PER_BOX_WIDTH 16
+#define PER_BOX_HEIGHT 16
+
+#define ID_ANI_BOX 10000
+
+
 class CBox : public CGameObject
 {
 protected:
-	int length;				// Unit: cell 
-	float cellWidth;
-	float cellHeight;
-	int spriteIdBegin, spriteIdMiddle, spriteIdEnd;
+	// int length;				// Unit: cell 
+	// float cellWidth;
+	// float cellHeight;
+	// int spriteIdBegin, spriteIdMiddle, spriteIdEnd;
+	int rowNum, columnNum;
 
 public:
-	CBox(float x, float y,
-		float cell_width, float cell_height, int length,
-		int sprite_id_begin, int sprite_id_middle, int sprite_id_end) :CGameObject(x, y)
+	CBox(float x, float y, int rowNum, int columnNum) :CGameObject(x, y)
 	{
-		this->length = length;
-		this->cellWidth = cell_width;
-		this->cellHeight = cell_height;
-		this->spriteIdBegin = sprite_id_begin;
-		this->spriteIdMiddle = sprite_id_middle;
-		this->spriteIdEnd = sprite_id_end;
+		this->x = x;
+		this->y = y;
+		this->rowNum = rowNum;
+		this->columnNum = columnNum;
 	}
 
 	float GetStartX() { return x; }
-	float GetEndX() { return x + length * cellWidth; }
+	float GetEndX() { return x + rowNum * PER_BOX_WIDTH; }
 
 	void Render();
 	void Update(DWORD dt) {}
--- 05-SceneManager/PlayScene.cpp
+++ 05-SceneManager/PlayScene.cpp
@@ -163,18 +163,17 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 
 	case OBJECT_TYPE_BOX:
 	{
-
-		float cell_width = (float)atof(tokens[3].c_str());
-		float cell_height = (float)atof(tokens[4].c_str());
-		int length = atoi(tokens[5].c_str());
-		int sprite_begin = atoi(tokens[6].c_str());
-		int sprite_middle = atoi(tokens[7].c_str());
-		int sprite_end = atoi(tokens[8].c_str());
+		int rowNum = atoi(tokens[3].c_str());
+		int columnNum = atoi(tokens[4].c_str());
+		// float cell_width = (float)atof(tokens[3].c_str());
+		// float cell_height = (float)atof(tokens[4].c_str());
+		// int length = atoi(tokens[5].c_str());
+		// int sprite_begin = atoi(tokens[6].c_str());
+		// int sprite_middle = atoi(tokens[7].c_str());
+		// int sprite_end = atoi(tokens[8].c_str());
 
 		obj = new CBox(
-			x, y,
-			cell_width, cell_height, length,
-			sprite_begin, sprite_middle, sprite_end
+			x, y, rowNum, columnNum
 		);
 
 		break;
