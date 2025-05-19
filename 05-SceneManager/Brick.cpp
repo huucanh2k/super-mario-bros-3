d70a4be2d4a97f473d78ccf81856ad65516cdb8a
Cảnh Trần Hữu <tranhuucanh2000@gmail.com>
2025-05-19 13:35:37 +0700

    Change name and refactor Brick

--- 05-SceneManager/Brick.cpp
+++ 05-SceneManager/Brick.cpp
@@ -3,14 +3,44 @@
 void CBrick::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
-	animations->Get(ID_ANI_BRICK)->Render(x, y);
+	int id = GetAnimationid();
+	if (id == -1)
+	{
+		return;
+	}
+	animations->Get(id)->Render(x, y);
 	//RenderBoundingBox();
 }
 
-void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
+int CBrick::GetAnimationid()
 {
-	l = x - BRICK_BBOX_WIDTH/2;
-	t = y - BRICK_BBOX_HEIGHT/2;
+	if (type == BRICK_TYPE_FLOOR_BOTTOM_LEFT)
+		return ID_ANI_BRICK_FLOOR_BOTTOM_LEFT;
+	else if (type == BRICK_TYPE_FLOOR_BOTTOM_MIDDLE)
+		return ID_ANI_BRICK_FLOOR_BOTTOM_MIDDLE;
+	else if (type == BRICK_TYPE_FLOOR_BOTTOM_RIGHT)
+		return ID_ANI_BRICK_FLOOR_BOTTOM_RIGHT;
+	else if (type == BRICK_TYPE_FLOOR_TOP_LEFT)
+		return ID_ANI_BRICK_FLOOR_TOP_LEFT;
+	else if (type == BRICK_TYPE_FLOOR_TOP_MIDDLE)
+		return ID_ANI_BRICK_FLOOR_TOP_MIDDLE;
+	else if (type == BRICK_TYPE_FLOOR_TOP_RIGHT)
+		return ID_ANI_BRICK_FLOOR_TOP_RIGHT;
+	else if (type == BRICK_TYPE_CONVEX)
+		return ID_ANI_BRICK_CONVEX;
+	else if (type == BRICK_TYPE_CLOUD)
+		return ID_ANI_BRICK_CLOUD;
+	else if (type == BRICK_TYPE_QUESTION)
+		return ID_ANI_QUESTION_BRICK;
+	else
+		return -1;
+
+}
+
+void CBrick::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - BRICK_BBOX_WIDTH / 2;
+	t = y - BRICK_BBOX_HEIGHT / 2;
 	r = l + BRICK_BBOX_WIDTH;
 	b = t + BRICK_BBOX_HEIGHT;
 }
