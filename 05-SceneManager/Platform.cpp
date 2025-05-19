ab07ef281503b2fba47c3d63663c866512a6e924
Cảnh Trần Hữu <tranhuucanh2000@gmail.com>
2025-05-19 13:32:24 +0700

    Refactor Platform

--- 05-SceneManager/Platform.cpp
+++ 05-SceneManager/Platform.cpp
@@ -31,19 +31,21 @@ void CPlatform::RenderBoundingBox()
 
 void CPlatform::Render()
 {
-	/*if (this->length <= 0) return; 
+	if (this->length <= 0) return; 
 	float xx = x; 
 	CSprites * s = CSprites::GetInstance();
 
 	s->Get(this->spriteIdBegin)->Draw(xx, y);
 	xx += this->cellWidth;
+
 	for (int i = 1; i < this->length - 1; i++)
 	{
 		s->Get(this->spriteIdMiddle)->Draw(xx, y);
 		xx += this->cellWidth;
 	}
-	if (length>1)
-		s->Get(this->spriteIdEnd)->Draw(xx, y);*/
+
+	if (length > 1)
+		s->Get(this->spriteIdEnd)->Draw(xx, y);
 
 	RenderBoundingBox();
 }
@@ -60,6 +62,5 @@ void CPlatform::GetBoundingBox(float& l, float& t, float& r, float& b)
 int CPlatform::IsDirectionColliable(float nx, float ny)
 {
 	if (nx == 0 && ny == -1) return 1;
-	if (nx != 0 && ny == 0) return 1;
 	else return 0;
 }
