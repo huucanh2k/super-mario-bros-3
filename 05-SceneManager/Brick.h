22169226badc49fb514b1b8848927633c477dc68
Vo Minh Vu <146900726+Vuvom1@users.noreply.github.com>
2025-05-25 00:49:40 +0700

    Implement activation behavior for ShinyBrick with particle effects

--- 05-SceneManager/Brick.h
+++ 05-SceneManager/Brick.h
@@ -16,6 +16,14 @@
 #define ID_ANI_BRICK_FLOOR_TOP_MIDDLE 10004
 #define ID_ANI_BRICK_FLOOR_TOP_RIGHT 10005
 
+#define ID_ANI_BRICK_CONVEX 10010
+
+#define ID_ANI_QUESTION_BRICK 10020
+#define ID_ANI_QUESTION_BRICK_BOUNCE 10023
+#define ID_ANI_QUESTION_BRICK_INACTIVE 10024
+
+#define ID_ANI_SHINY_BRICK 10030
+
 #define BRICK_TYPE_NORMAL 6
 #define BRICK_TYPE_FLOOR_BOTTOM_LEFT 0
 #define BRICK_TYPE_FLOOR_BOTTOM_MIDDLE 1
@@ -23,11 +31,11 @@
 #define BRICK_TYPE_FLOOR_TOP_LEFT 3
 #define BRICK_TYPE_FLOOR_TOP_MIDDLE 4
 #define BRICK_TYPE_FLOOR_TOP_RIGHT 5
+#define BRICK_TYPE_CONVEX 10
 
 #define BRICK_TYPE_DEATH 100 //Touch this brick and mario die
 
-#define BRICK_TYPE_CONVEX 10
-#define ID_ANI_BRICK_CONVEX 10010
+
 
 #define BRICK_TYPE_CLOUD 7
 #define ID_ANI_BRICK_CLOUD 10007
@@ -35,9 +43,7 @@
 #define BRICK_TYPE_QUESTION 20
 #define BRICK_TYPE_QUESTION_INACTIVE 21
 
-#define ID_ANI_QUESTION_BRICK 10020
-#define ID_ANI_QUESTION_BRICK_INACTIVE 10024
-
+#define BRICK_TYPE_SHINY 30
 class CBrick : public CGameObject {
 protected:
 	int type;
@@ -48,5 +54,6 @@ public:
 	int GetType() { return type; }
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	int IsBlocking() { return type != BRICK_TYPE_DEATH; }
+	int IsCollidable() { return 1; }
 	int GetAnimationid();
 };
