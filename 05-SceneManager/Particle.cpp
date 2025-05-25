1ad4a8e785b29b729e34cf11b3a3d6d70a08727a
Cảnh Trần Hữu <tranhuucanh2000@gmail.com>
2025-05-25 07:05:56 +0700

    Add particle

--- 05-SceneManager/Particle.cpp
+++ 05-SceneManager/Particle.cpp
@@ -2,8 +2,19 @@
 
 void CParticle::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
+    ULONGLONG now = GetTickCount64();
+
+	if (type == PARTICLE_TYPE_BRICK || type == PARTICLE_TYPE_BRICK_LOW || 
+        type == PARTICLE_TYPE_BRICK_REVERSED || type == PARTICLE_TYPE_BRICK_REVERSED_LOW)
+	{
+		if (now - creationTime >= PARTICLE_BRICK_LIFE_TIME/3)
+		{
+			vy = 0.26f;
+		}
+	}
+
     // Automatically delete the particle after its lifetime
-    if (GetTickCount64() - creationTime >= lifetime)
+    if (now - creationTime >= lifetime)
     {
         this->Delete();
     }
@@ -16,9 +27,36 @@ void CParticle::Render()
 {
     int aniID = -1;
 	if (type == PARTICLE_TYPE_POINT)
-		aniID = ID_ANI_PARTICLE_POINT;
+    {
+		if (point == 100)
+			aniID = ID_ANI_PARTICLE_POINT_100;
+		else if (point == 200)
+			aniID = ID_ANI_PARTICLE_POINT_200;
+		else if (point == 400)
+			aniID = ID_ANI_PARTICLE_POINT_400;
+		else if (point == 800)
+			aniID = ID_ANI_PARTICLE_POINT_800;
+		else if (point == 1000)
+			aniID = ID_ANI_PARTICLE_POINT_1000;
+		else if (point == 2000)
+			aniID = ID_ANI_PARTICLE_POINT_2000;
+		else if (point == 4000)
+			aniID = ID_ANI_PARTICLE_POINT_4000;
+		else if (point == 8000)
+			aniID = ID_ANI_PARTICLE_POINT_8000;
+		else if (point == 10000 || point == 0)
+			aniID = ID_ANI_PARTICLE_POINT_1UP;
+		else
+			aniID = ID_ANI_PARTICLE_POINT_100;
+    }
 	else if (type == PARTICLE_TYPE_HIT)
 		aniID = ID_ANI_PARTICLE_HIT;
+	else if (type == PARTICLE_TYPE_BRICK || type == PARTICLE_TYPE_BRICK_LOW)
+		aniID = ID_ANI_PARTICLE_BRICK;
+	else if (type == PARTICLE_TYPE_BRICK_REVERSED || type == PARTICLE_TYPE_BRICK_REVERSED_LOW)
+		aniID = ID_ANI_PARTICLE_BRICK_REVERSED;
+	else if (type == PARTICLE_TYPE_SMOKE)
+		aniID = ID_ANI_PARTICLE_SMOKE;
 
 	CAnimations* animations = CAnimations::GetInstance();
 	animations->Get(aniID)->Render(x, y);
--- 05-SceneManager/Particle.h
+++ 05-SceneManager/Particle.h
@@ -4,12 +4,31 @@
 
 #define PARTICLE_TYPE_HIT 1
 #define PARTICLE_TYPE_POINT 2
+#define PARTICLE_TYPE_BRICK 3
+#define PARTICLE_TYPE_BRICK_LOW 4
+#define PARTICLE_TYPE_BRICK_REVERSED 5
+#define PARTICLE_TYPE_BRICK_REVERSED_LOW 6
+#define PARTICLE_TYPE_SMOKE 7
 
 #define PARTICLE_HIT_LIFE_TIME 250
 #define PARTICLE_POINT_LIFE_TIME 1000
+#define PARTICLE_BRICK_LIFE_TIME 800
+#define PARTICLE_SMOKE_LIFE_TIME 240
 
+#define ID_ANI_PARTICLE_SMOKE 302
 #define ID_ANI_PARTICLE_HIT 12000
-#define ID_ANI_PARTICLE_POINT 12010
+#define ID_ANI_PARTICLE_POINT_100 12010
+#define ID_ANI_PARTICLE_POINT_200 12011
+#define ID_ANI_PARTICLE_POINT_400 12012
+#define ID_ANI_PARTICLE_POINT_800 12013
+#define ID_ANI_PARTICLE_POINT_1000 12014
+#define ID_ANI_PARTICLE_POINT_2000 12015
+#define ID_ANI_PARTICLE_POINT_4000 12016
+#define ID_ANI_PARTICLE_POINT_8000 12017
+#define ID_ANI_PARTICLE_POINT_1UP 12018
+#define ID_ANI_PARTICLE_BRICK 12020
+#define ID_ANI_PARTICLE_BRICK_REVERSED 12021
+
 
 class CParticle : public CGameObject
 {
@@ -17,9 +36,10 @@ protected:
     DWORD lifetime; // Duration the particle will exist
     DWORD creationTime; // Time when the particle was created
 	int type; // Type of particle (e.g., smoke, explosion, etc.)
+    int point; // To choose the correct point particle to show
 
 public:
-    CParticle(float x, float y, int type) : CGameObject(x, y)
+    CParticle(float x, float y, int type, int p = 0) : CGameObject(x, y)
     {
         this->creationTime = GetTickCount64();
 		this->type = type;
@@ -27,12 +47,38 @@ public:
 			this->vx = 0.f;
 			this->vy = -0.05f;
             this->lifetime = PARTICLE_POINT_LIFE_TIME;
+			this->point = p;
 		}
 		else if (type == PARTICLE_TYPE_HIT) {
             this->vx = 0.f;
             this->vy = 0.f;
             this->lifetime = PARTICLE_HIT_LIFE_TIME;
 		}
+        else if (type == PARTICLE_TYPE_BRICK) {
+            this->vx = 0.07f;
+            this->vy = -0.26f;
+            this->lifetime = PARTICLE_BRICK_LIFE_TIME;
+		}
+		else if (type == PARTICLE_TYPE_BRICK_LOW) {
+			this->vx = 0.07f;
+			this->vy = -0.13f;
+			this->lifetime = PARTICLE_BRICK_LIFE_TIME;
+		}
+		else if (type == PARTICLE_TYPE_BRICK_REVERSED) {
+			this->vx = -0.07f;
+			this->vy = -0.26f;
+			this->lifetime = PARTICLE_BRICK_LIFE_TIME;
+		}
+        else if (type == PARTICLE_TYPE_BRICK_REVERSED_LOW) {
+            this->vx = -0.07f;
+            this->vy = -0.13f;
+            this->lifetime = PARTICLE_BRICK_LIFE_TIME;
+        }
+        else if (type == PARTICLE_TYPE_SMOKE) {
+            this->vx = 0.f;
+            this->vy = -0.f;
+            this->lifetime = PARTICLE_SMOKE_LIFE_TIME;
+        }
         else {
             this->vx = 0.f;
             this->vy = 0.f;
